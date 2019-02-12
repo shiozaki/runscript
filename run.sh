503920b5af83cfd798f5b8907f96222f104dce3d
Toru Shiozaki <shiozaki.toru@gmail.com>
2019-02-12 02:40:52 +0000

    minor

--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ echo ppn: $ppn
 export OMP_NUM_THREADS=$ppn
 
 # execute
+ls -ltra /opt/intel/compilers_and_libraries/linux/mpi/bin64/mpivars.sh
+source /opt/intel/compilers_and_libraries/linux/mpi/bin64/mpivars.sh
 cmd=$(eval echo "${SHIPYARD_SINGULARITY_COMMAND}")
 echo $cmd
 mpirun -hosts $AZ_BATCH_HOST_LIST -perhost 1 -np $nodes $cmd /usr/bin/a.out
